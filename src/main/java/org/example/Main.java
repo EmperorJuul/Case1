cfdae7c4a70e3d4b07cadee6ab6c428a70b07334
konij11 <j.konings@belastingdienst.nl>
2023-05-09 14:47:06 +0200

    Klein startscherm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,28 @@
 package org.example;
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true){
+            startscherm();
+        }
+    }
+
+    public static void startscherm(){
+        System.out.println("""
+                    WELKOM OP BELASTINGPLAATS    
+                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                X                               X
+                X   1. Log in                   X
+                X   2. Sluit applicatie         X
+                X                               X
+                X                               X
+                X                               X
+                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                """);
+        Scanner scanner = new Scanner(System.in);
+        int keuze = scanner.nextInt();
     }
 }
